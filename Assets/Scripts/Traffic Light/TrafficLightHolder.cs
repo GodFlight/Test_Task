ca59404ceec81ec4453209eef6be2bbaba8aed9e
GodFlight <prokushevmaksim@gmail.com>
2021-01-29 13:22:35 +0300

    Added: traffic light holder(ui)

---
+++ Assets/Scripts/Traffic Light/TrafficLightHolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TrafficLight.UI
+{
+	public class TrafficLightHolder : MonoBehaviour
+	{
+		[SerializeField]
+		private Image[] trafficLights;
+
+		public void PrepareTrafficLights(TrafficLightSettings[] settings)	
+		{
+			for (int i = 0; i < settings.Length; i++)
+			{
+				trafficLights[i].gameObject.SetActive(true);
+				trafficLights[i].color = settings[i].waitingColorOfLight;
+			}
+		}
+	}
+}
+
