b13cf7a98ac6d544dc69189151340fe8ea6e8364
GodFlight <prokushevmaksim@gmail.com>
2021-01-29 13:00:21 +0300

    Added: Traffic light setting

---
+++ Assets/Scripts/Traffic Light/TrafficLightSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace TrafficLight
+{
+	[CreateAssetMenu (menuName = "Traffic Light Settings")]
+	public class TrafficLightSettings : ScriptableObject
+	{
+		[Range(0f, 25f)]
+		public float duration;
+		public bool inUISpace;
+		public bool inRealSpace = false;
+		public Sprite spriteOfTrafficLight;
+		public Color colorOfLight;
+	}
+}
+
